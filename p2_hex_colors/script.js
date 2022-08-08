d20f20e080b73d19f4517813fcddbc8433391d24
DaniyalAkbar <daniyalakbar1217@gmail.com>
2022-08-08 20:13:40 +0500

    2nd project started

---
+++ p2_hex_colors/script.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    var hex_numbers = ['0', '1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    var hexcode = '';
+
+    for(var i=0; i<6; i++){
+        
+    }
+
+}
